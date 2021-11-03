238bc254b7dafe5440571eb81784e45852e0bfea
PiotrGrzyb <piotrg6711@gmail.com>
2021-11-03 14:48:44 +0100

    Update Scatter.py

--- Scatter.py
+++ Scatter.py
@@ -7,11 +7,35 @@ def main():
     telescopeDataSet.columns = ['fLength', 'fWidth', 'fSize', 'fConc', 'fConc1', 'fAsym',
                                 'fM3Long', 'fM3Trans', 'fAlpha', 'fDist', 'class']
 
-    #fig = px.scatter(telescopeDataSet, x="fLength", y="class", size="Energy", color="Beats Per Minute (BPM)",
-    #                 hover_name="Title")
-
     fig = px.scatter(telescopeDataSet, x="class", y="fLength")
     fig.show()
+
+    fig2 = px.scatter(telescopeDataSet, x="class", y="fWidth")
+    #fig2.show()
+
+    fig3 = px.scatter(telescopeDataSet, x="class", y="fSize")
+    #fig3.show()
+
+    fig4 = px.scatter(telescopeDataSet, x="class", y="fConc")
+    #fig4.show()
+
+    fig5 = px.scatter(telescopeDataSet, x="class", y="fConc1")
+    #fig5.show()
+
+    fig6 = px.scatter(telescopeDataSet, x="class", y="fAsym")
+    #fig6.show()
+
+    fig7 = px.scatter(telescopeDataSet, x="class", y="fM3Long")
+    #fig7.show()
+
+    fig8 = px.scatter(telescopeDataSet, x="class", y="fM3Trans")
+    #fig8.show()
+
+    fig9 = px.scatter(telescopeDataSet, x="class", y="fAlpha")
+    #fig9.show()
+
+    fig10 = px.scatter(telescopeDataSet, x="class", y="fDist")
+    #fig10.show()
 
 if __name__ == '__main__':
     main()
