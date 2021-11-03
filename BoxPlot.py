5795faa451fb6d69c50563f19df8dce7ba4a58e4
PiotrGrzyb <piotrg6711@gmail.com>
2021-11-03 15:23:33 +0100

    Value and box plots

---
+++ BoxPlot.py
@@ -0,0 +1,26 @@
+import pandas
+import plotly.express as px
+
+
+def main():
+    telescopeDataSet = pandas.read_csv('../Data-analysis-in-python-/magic04(gamma-1, hardon-0).data')
+    telescopeDataSet.columns = ['fLength', 'fWidth', 'fSize', 'fConc', 'fConc1', 'fAsym',
+                                'fM3Long', 'fM3Trans', 'fAlpha', 'fDist', 'class']
+
+    fLenghtBox = px.box(data_frame=telescopeDataSet, y='fLength')
+    fWidthBox = px.box(data_frame=telescopeDataSet, y='fWidth')
+    fSizeBox = px.box(data_frame=telescopeDataSet, y='fSize')
+    fConcBox = px.box(data_frame=telescopeDataSet, y='fConc')
+    fConc1Box = px.box(data_frame=telescopeDataSet, y='fConc1')
+    fAsymBox = px.box(data_frame=telescopeDataSet, y='fAsym')
+    fM3LongBox = px.box(data_frame=telescopeDataSet, y='fM3Long')
+    fM3TransBox = px.box(data_frame=telescopeDataSet, y='fM3Trans')
+    fAlphaBox = px.box(data_frame=telescopeDataSet, y='fAlpha')
+    fDistBox = px.box(data_frame=telescopeDataSet, y='fDist')
+    classBox = px.box(data_frame=telescopeDataSet, y='class')
+
+    fLenghtBox.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ ValuePlot.py
@@ -0,0 +1,26 @@
+import pandas
+import plotly.express as px
+
+
+def main():
+    telescopeDataSet = pandas.read_csv('../Data-analysis-in-python-/magic04(gamma-1, hardon-0).data')
+    telescopeDataSet.columns = ['fLength', 'fWidth', 'fSize', 'fConc', 'fConc1', 'fAsym',
+                                'fM3Long', 'fM3Trans', 'fAlpha', 'fDist', 'class']
+
+    fLenghtLine = px.line(data_frame=telescopeDataSet, y='fLength')
+    fWidthLine = px.line(data_frame=telescopeDataSet, y='fWidth')
+    fSizeLine = px.line(data_frame=telescopeDataSet, y='fSize')
+    fConcLine = px.line(data_frame=telescopeDataSet, y='fConc')
+    fConc1Line = px.line(data_frame=telescopeDataSet, y='fConc1')
+    fAsymLine = px.line(data_frame=telescopeDataSet, y='fAsym')
+    fM3LongLine = px.line(data_frame=telescopeDataSet, y='fM3Long')
+    fM3TransLine = px.line(data_frame=telescopeDataSet, y='fM3Trans')
+    fAlphaLine = px.line(data_frame=telescopeDataSet, y='fAlpha')
+    fDistLine = px.line(data_frame=telescopeDataSet, y='fDist')
+    classLine = px.line(data_frame=telescopeDataSet, y='class')
+
+    fDistLine.show()
+
+
+if __name__ == '__main__':
+    main()
